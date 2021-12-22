43575013168a1125f7f6701f69d5487580ad26b7
ErykUeberhan <eryk.ueberhan@gmail.com>
2021-12-22 02:10:37 +0100

    Code cleaning

--- src/hooks/GetRooms.tsx
+++ src/hooks/GetRooms.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useQuery } from 'react-query';
 import { TRooms } from '../lib/types/TRooms';
 
-function GetRooms(hotelId:any) {
+const GetRooms = (hotelId:any) => {
     const { isLoading, error, data } = useQuery(hotelId, () => axios.get<TRooms>("https://obmng.dbm.guestline.net/api/roomRates/OBMNG/"+hotelId));
     return { isLoading, error, data };
 }
